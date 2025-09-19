49c646bc8c1e6abc2e8a210d6fede917d63c8636
Vimalcoder <webcoder640@gmail.com>
2025-09-19 10:41:08 +0530

    latest git push

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
     NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000',
   },
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose']
-  }
+   serverExternalPackages: ['mongoose'],
 }
 
 export default nextConfig
